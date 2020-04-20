2143f6f9fa015fe5a60fa4427d9135a764e585ea
Johnny Nia <johnny.nia@gmail.com>
2020-04-20 15:15:05 +0200

    Customize timing.

--- app/models/poll_page.rb
+++ app/models/poll_page.rb
@@ -1,4 +1,6 @@
 class PollPage
+  @@counter = 0
+
   def self.perform(page: nil)
     html = page.html
     sha2_hash = page.sha2_hash
@@ -18,7 +20,18 @@ class PollPage
   end
 
   def self.perform_all
+    @@counter += 1
+
     Page.all.shuffle.each do |page|
+      # 1 Instrumentor
+      # 2 BAG
+      # 3 Kt. ZH
+      hour = Time.now.hour
+      next if hour < 8 || hour > 19
+      next if page.id > 3 && @@counter % 13 != 1
+
+      logger.info "Yeah: Polling #{page.title}"
+
       begin
         self.perform(page: page)
       rescue
